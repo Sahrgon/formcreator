26049a1dc0c2da2f53f264ac65c64f4476d2304a
Sahrgon <Sahrgon@users.noreply.github.com>
2016-05-12 15:20:35 +0200

    New function for encoding fields values. Encoding is now done before checking if field is visible.

--- ajax/showfields.php
+++ ajax/showfields.php
@@ -2,21 +2,6 @@
 include ('../../../inc/includes.php');
 
 $currentValues  = json_decode(stripslashes($_POST['values']), true);
-foreach ($currentValues as &$value) {
-   if (is_array($value)) {
-      foreach ($value as &$sub_value) {
-         $sub_value = plugin_formcreator_encode($sub_value);
-      }
-   } elseif (is_array(json_decode($value))) {
-      $tab = json_decode($value);
-      foreach ($tab as &$sub_value) {
-         $sub_value = plugin_formcreator_encode($sub_value);
-      }
-      $value = json_encode($tab);
-   } else {
-      $value = plugin_formcreator_encode($value);
-   }
-}
 $questionToShow = array();
 foreach ($currentValues as $id => $value) {
    $questionToShow[$id] = PluginFormcreatorFields::isVisible($id, $currentValues);
